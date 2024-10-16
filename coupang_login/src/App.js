9c0f7af1945de52383e7d03b7ddfed3c75b0d99a
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2024-10-16 20:59:39 +0900

    header, footer html 작업

--- coupang_login/src/App.js
+++ coupang_login/src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, Link, useNavigate, outlet} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+        
+      </Routes>
+      <Footer/>
     </div>
   );
 }
 
+function Header(){
+  return(
+    <div>
+      <img src='#'></img>
+      <div className='tab'>
+        <ul>
+          <li><span>이메일 로그인</span></li>
+          <li><span>휴대폰번호 로그인</span></li>
+          <li><span>QR코드 로그인</span></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function Footer(){
+  return(
+    <div>
+      <div className='footer-contentbox'>
+        <p className='footer-contentbox-content'>법인 고객이신가요?<br/>
+        사업자 회원으로 전용 특가 혜택을 누려보세요.</p>
+        <a href='#'>쿠팡비즈 간편가입</a>
+      </div>
+      <div className='footer-copyright'>
+        <p className='footer-copyright-content'>©Coupang Corp. All rights reserved.</p>
+      </div>
+    </div>
+  )
+}
 export default App;
