4e535032d8d78783acfba2dcd7db24f731f73c52
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2024-10-16 18:53:16 +0900

    personal blog

---
+++ personal_blog/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes, Route, Link, useNavigate, outlet} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Routes>
+      </Routes>
+    </div>
+  );
+}
+
+function Header(){
+  
+}
+
+export default App;
